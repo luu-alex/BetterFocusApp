3060e8e69acecda5573b586a6496650b93e37f2a
luu-alex <luu.alex98@gmail.com>
2019-07-01 14:24:04 -0400

    added a test button component

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component} from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import MyButton from './src/components/MyButton.tsx';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  render() {
+    return (
+        <View style={styles.container}>
+          <Text> working on your!</Text>
+          <MyButton />
+        </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
