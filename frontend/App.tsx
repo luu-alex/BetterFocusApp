80bdcdc57b2ea40c54ca7b382df2288b9b3384cf
Ali Raza <aliraza@ibm.com>
2019-07-04 21:54:34 -0400

    todos page with bottom nav

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -3,7 +3,13 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import MyButton from './src/components/MyButton';
 import TodoInput from './src/components/todoInput';
 import AppBar from './src/components/AppBar'
-import { Provider as PaperProvider } from 'react-native-paper';
+import BottomNav from './src/components/bottomNavbar'
+import {
+    Appbar,
+    Provider as PaperProvider,
+    DarkTheme,
+    DefaultTheme,
+  } from 'react-native-paper';
 
 
 export default class App extends Component {
@@ -12,6 +18,7 @@ export default class App extends Component {
         <PaperProvider>
             <AppBar />
             <TodoInput />
+            <BottomNav />
         </PaperProvider>
         
         );
@@ -31,5 +38,11 @@ const styles = StyleSheet.create({
   },
   inputContainerStyle: {
     margin: 8,
+  },
+  bottom: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
 });
